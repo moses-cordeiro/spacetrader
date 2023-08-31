6a6cbc1598b28f4d35c3e2a29d86ac8a713bbe02
Moses Cordeiro <mosescordeiro1@gmail.com>
2023-08-30 23:26:18 -0400

    Better request function, can also now obtain contracts

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { SpaceTrader } from "./modules/api";
 import 'dotenv/config'
+import fs from 'fs';
 
 let trader = new SpaceTrader(process.env.ST_API_TOKEN, process.env.ST_API_URL);
 
+function dirty_logger(fileName:string, data: any) {
+    let jsonData = JSON.stringify(data, null, 2)
+    console.log(jsonData);
+    fs.writeFileSync('./results/'+fileName, jsonData);
+}
 
-trader.get_agent().then((data) => { console.log(data); }); 
+trader.get_my_waypoint().then((data) => { 
+    dirty_logger('get_my_waypoint.json', data)
+}); 
+
+trader.get_my_contracts().then((data) => { 
+    dirty_logger('get_my_contracts.json', data)
+});
--- modules/api.ts
+++ modules/api.ts
@@ -7,19 +7,38 @@ export class SpaceTrader {
     this.api_token = user_token;
   }
 
-  async getRequest(url = "") {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${this.api_token}`
-      }    
-    });
+  async request(endpoint: string, method: string = "GET", data: any = null) {
+    type args = {[key: string] : any}
+    const fetchArgs: args = {};
+
+    fetchArgs.method = method
+    fetchArgs.headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${this.api_token}`
+    }  
+
+    if( !(typeof data === 'undefined' || data === null) ){ 
+      fetchArgs.body = JSON.stringify(data); 
+    }
+
+    const response = await fetch(this.url+endpoint, fetchArgs);
+
+    // TODO - Handle errors
+
     return response.json();
   }
 
-  async get_agent() {
-    let endpoint:string = this.url + '/my/agent';
-    return await this.getRequest(endpoint);;
+  async get_my_agent() { return await this.request('/my/agent'); }
+
+  async get_my_contracts() { return await this.request('/my/contracts'); }
+
+  async get_my_waypoint() {
+    let agentData = (await this.get_my_agent()).data;
+    let splitWaypoint = agentData.headquarters.split('-');
+    let endpoint:string = '/systems/' + splitWaypoint[0] + '-' + splitWaypoint[1] + '/waypoints/' + agentData.headquarters;
+    return await this.request(endpoint);
   }
+
+  // NEXT STEPS -> https://docs.spacetraders.io/quickstart/first-mission
+  // Need to accept contract and progress on mining mission.
 }
